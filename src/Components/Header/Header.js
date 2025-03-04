a8d4a911cf7b54d11df2aa81e79cdbb5d0dd77eb
Samuel Pouard <thosam73000@gmail.com>
2025-03-05 00:05:34 +0100

    FileTransformer
    header modifier

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,11 +5,11 @@ const Header = () => {
       <header>
         <div className="header-container">
         <h1 className="titre-header">FileTransformer</h1>
-        <p>Fusionner PDF</p>
-        <p>Diviser PDF</p>
-        <p>Compresser PDF</p>
-        <p>Convertire PDF</p>
-        <p>Touts les outils PDF</p>
+        <p className="texte-header">Fusionner PDF</p>
+        <p className="texte-header">Diviser PDF</p>
+        <p className="texte-header">Compresser PDF</p>
+        <p className="texte-header">Convertire PDF</p>
+        <p className="texte-header">Touts les outils PDF</p>
         </div>
         <div className="btn-container-header">
           <button className="btn-header">S'inscrire</button>
